a691d85b0dc080aa45b9e4c54ba859ba2e9437b4
angeella <a.andreella8@gmail.com>
2020-04-06 10:59:48 +0200

    up package

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,49 +6,6 @@
 
 using namespace Rcpp;
 
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _ARIeeg_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _ARIeeg_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _ARIeeg_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _ARIeeg_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 // rowSortC
 arma::mat rowSortC(arma::mat X);
 RcppExport SEXP _ARIeeg_rowSortC(SEXP XSEXP) {
@@ -62,10 +19,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_ARIeeg_rcpparma_hello_world", (DL_FUNC) &_ARIeeg_rcpparma_hello_world, 0},
-    {"_ARIeeg_rcpparma_outerproduct", (DL_FUNC) &_ARIeeg_rcpparma_outerproduct, 1},
-    {"_ARIeeg_rcpparma_innerproduct", (DL_FUNC) &_ARIeeg_rcpparma_innerproduct, 1},
-    {"_ARIeeg_rcpparma_bothproducts", (DL_FUNC) &_ARIeeg_rcpparma_bothproducts, 1},
     {"_ARIeeg_rowSortC", (DL_FUNC) &_ARIeeg_rowSortC, 1},
     {NULL, NULL, 0}
 };
