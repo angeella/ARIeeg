5fd633a9a55478f01f5f464f128028fa3d945d63
angeella <a.andreella8@gmail.com>
2020-04-15 16:27:57 +0200

    update plotNull

--- R/plotNullDistributionEEG.R
+++ R/plotNullDistributionEEG.R
@@ -44,6 +44,7 @@ plotNullDistributionEEG <- function(P,family="simes",alpha = 0.1, ct = c(0,1), p
       lambdaO <- lambdaOpt(pvalues = pvalues_ord,family=family,ct=ct,alpha=alpha, delta = delta)
       cvO<- cv(pvalues = pvalues_ord, family = family, alpha = alpha, lambda = lambdaO, delta = delta)
       lines(cvO, lwd =2, col= cols)
+      
     }
     firstup <- function(x) {
       substr(x, 1, 1) <- toupper(substr(x, 1, 1))
@@ -51,6 +52,7 @@ plotNullDistributionEEG <- function(P,family="simes",alpha = 0.1, ct = c(0,1), p
     }
     lcvV <- Vectorize(lcv,vectorize.args = c("family", "delta", "cols"))
     cols = rainbow(length(family))
+    cols[3] = "#FF0099FF"
     png(paste0(path,"/", name, ".png")) 
     plot(pvalues_ord[1,], type = 'l', col = ' green', xlab = expression(i), ylab = expression(p[(i)]))
     for(i in 2:nrow(pvalues_ord)){
@@ -62,8 +64,9 @@ plotNullDistributionEEG <- function(P,family="simes",alpha = 0.1, ct = c(0,1), p
     #lines(cvO, col= 'blue', lwd =2)
     mapply(lcv, family, delta, cols)
     family <- firstup(family)
-    legend('top',legend=c(sapply(c(1:length(family)), 
-                                 function(x) as.expression(bquote(~ .(family[x]) ~ delta == .(delta[x]) ))), 
+    fn = sum(family!="beta" | family!="higher.criticism")
+    legend('top',legend=c(sapply(c(1:fn), 
+                                 function(x) if(family[x]=="Beta" | family[x]=="Higher.criticism") {paste0(" ",family[x])}else{as.expression(bquote(~ .(family[x]) ~ delta == .(delta[x]) ))} ), 
                           " Observed Pvalues"), col= c(cols, "green"),lwd =2)
     
     dev.off()
